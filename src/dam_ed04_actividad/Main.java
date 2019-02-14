a0d3f4f6508eafcc8d6fa859c83fcf7f9a79b3ff
U_35007842 <U_35007842@DESKTOP-30JTBN9>
2019-02-14 12:19:03 +0000

    Segundo commit

--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -10,7 +10,7 @@ public class Main {
 public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
-        int examen;
+        int examen2;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         operativa_cuenta(cuenta1, 0);
